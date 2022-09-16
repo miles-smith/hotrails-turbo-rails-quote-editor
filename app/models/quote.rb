ada22bda3f6b4303dfcfefaf80c3f9c99b083e03
Miles Smith <miles-smith@users.noreply.github.com>
2022-09-16 15:47:11 +0100

    Go real-time with ActionCable

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,11 @@
 class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
+  broadcasts_to ->(quote) { :quotes }, inserts_by: :prepend
+  # NOTE: The above is syntactic sugar for implementing all of the below succinctly
+  # after_create_commit  -> { broadcast_prepend_later_to :quotes }
+  # after_update_commit  -> { broadcast_replace_later_to :quotes }
+  # after_destroy_commit -> { broadcast_remove_to :quotes }
+
   validates :name, presence: true
 end
