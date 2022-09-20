a03d1d9cc727609fdeb50a9d96c65a2ea86dd896
Miles Smith <miles-smith@users.noreply.github.com>
2022-09-20 08:38:43 +0100

    Ensure broadcasted quotes are constrained to the correct stream/channel

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,7 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  broadcasts_to ->(quote) { :quotes }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, :quotes] }, inserts_by: :prepend
   # NOTE: The above is syntactic sugar for implementing all of the below succinctly
   # after_create_commit  -> { broadcast_prepend_later_to :quotes }
   # after_update_commit  -> { broadcast_replace_later_to :quotes }
